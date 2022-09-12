f76c85ddd6e268adf057bd1f6621dfc2bd6c738f
Robert Moritz <robert@nott-studios.co.uk>
2022-09-12 21:37:37 +1000

    feat: improved bg placement

--- packages/ui/AboveFold.ts
+++ packages/ui/AboveFold.ts
@@ -4,4 +4,6 @@ export const AboveFold = styled.div<{ bgimage: string }>`
   padding: 2rem;
   min-height: 100vh;
   background-image: url("${(p) => p.bgimage}");
+  background-size: cover;
+  background-position: center;
 `;
