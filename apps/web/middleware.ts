a5f2277696e4dc4ad84e6886959e007f1f546f9b
Robert Moritz <robert@nott-studios.co.uk>
2022-09-29 07:12:15 +0100

    test: fixing cache bug

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,24 +1,29 @@
-import { NextResponse } from "next/server";
+import { NextFetchEvent, NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest, event: NextFetchEvent) {
   const { nextUrl: url, geo } = req;
+
+  event.waitUntil(
+    fetch(`http://ip-api.com/json/${req.ip}`)
+      .then((res) => res.json())
+      .then((data) => console.log("data", data))
+  );
+
+  console.log("req", req);
   if (geo?.latitude && geo?.longitude) {
     url.searchParams.set("lat", geo.latitude);
     url.searchParams.set("lon", geo.longitude);
+    return NextResponse.rewrite(url);
   }
-
-  console.log("req", req);
 
   // TODO: might not be needed?
-  if (req.ip) {
-    const res = await fetch(`http://ip-api.com/json/${req.ip}`);
-    const data = await res.json();
+  // if (req.ip) {
+  //   const res = await fetch(`http://ip-api.com/json/${req.ip}`);
+  //   const data = await res.json();
 
-    console.log("data", data);
-  }
-
-  return NextResponse.rewrite(url);
+  //   console.log("data", data);
+  // }
 }
 
 export const config = {
